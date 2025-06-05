cef6744a0ddcc6da6665027af65fd71addb39660
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-05 17:28:32 +0700

    Tahap 3

--- stack.cpp
+++ stack.cpp
@@ -19,4 +19,16 @@ class Stack
 {
     private:
         Node *top; // Pointer to the top node of the stack
+    public:
+        Stack()
+        {
+            top = NULL; // Initialize the stack with a null top
+        }
+
+        // Push operation : Insert an element onto the top of the
+        int push(int value)
+        {
+            Node *newNode = new Node();
+        }
+
 }

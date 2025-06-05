d455b31ca539af4986cbbdcaef2417b57fb760fe
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-05 17:33:05 +0700

    Tahap 4

--- stack.cpp
+++ stack.cpp
@@ -29,6 +29,11 @@ class Stack
         int push(int value)
         {
             Node *newNode = new Node();
+            newNode->data = value; 
+            newNode->next = top; 
+            top = newNode;  
+            cout << "Push value:" << value << endl;
+            return value; 
         }
 
 }
